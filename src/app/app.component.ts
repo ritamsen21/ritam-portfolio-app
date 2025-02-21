eac6550cd30d01a3ec3b6d51ced44a98646bf625
ritamsen21 <62749847+ritamsen21@users.noreply.github.com>
2025-02-21 22:47:17 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ritam-portfolio-app';
+  isDarkMode = false;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.loadTheme();
+  }
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    this.applyTheme();
+    this.saveTheme();
+  }
+
+  private applyTheme() {
+    if (this.isDarkMode) {
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.renderer.removeClass(document.body, 'light-theme');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-theme');
+      this.renderer.addClass(document.body, 'light-theme');
+    }
+  }
+
+  private saveTheme() {
+    localStorage.setItem('darkMode', JSON.stringify(this.isDarkMode));
+  }
+
+  private loadTheme() {
+    const storedTheme = localStorage.getItem('darkMode');
+    if (storedTheme !== null) {
+      this.isDarkMode = JSON.parse(storedTheme);
+    }
+    this.applyTheme();
+  }
 }
