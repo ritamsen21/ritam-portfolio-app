8105f8ed3effdc7ad6910446cf10d4c3ba83f4f8
ritamsen21 <62749847+ritamsen21@users.noreply.github.com>
2025-02-22 19:23:41 +0530

    some changes 22/02

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
+
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -16,3 +18,4 @@ export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/home' }
 ];
+
